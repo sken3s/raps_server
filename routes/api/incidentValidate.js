f1e94287813a85d5ea1c476b37cf7526bab7a75f
Skenos Peniel <skenos3s@gmail.com>
2021-03-17 19:13:37 +0530

    changes

---
+++ routes/api/incidentValidate.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+let Event = require("../../models/event.model");
+let Accident = require("../../models/accident.model");
+let PoliceSession = require("../../models/policeSession.model");
