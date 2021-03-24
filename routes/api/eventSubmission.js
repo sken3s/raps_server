e854268b3747bf06a0824f9a63ab2fdd220b7363
Dulana Peiris <dulthar@gmail.com>
2021-03-24 21:48:22 +0530

    eTeam accid/event submissions updated. API endpoints changed

--- routes/api/eventSubmission.js
+++ routes/api/eventSubmission.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 let Event = require('../../models/event.model');
 let PoliceSession = require('../../models/policeSession.model');
+let ETeamSession = require('../../models/eTeamSession.model');
 
-//Submit (post request)
+//Submit (police)
 router.route('/submit').post((req, res) => {
   const { body } = req;
   const {
@@ -69,7 +70,72 @@ router.route('/submit').post((req, res) => {
         )
     });
 
-
+//Submit (eteam)
+router.route('/eteam/submit').post((req, res) => {
+    const { body } = req;
+    const {
+      datetime,
+      type ,
+      drivingSide ,
+      severity ,
+      kmPost  ,
+      suburb,
+      sessionToken
+  } = body;
+    //Data constraints
+    if(!datetime){
+        return res.send({
+            success:false,
+            message:'Error: Date/Time invalid.'
+        })}
+      if(!sessionToken|| sessionToken.length!=24){
+          return res.send({
+              success:false,
+              message:'Error: Session Token invalid.'
+          })}
+      //validating session
+      ETeamSession.find({   
+          _id:sessionToken, 
+          isDeleted:false
+      }, (err,sessions) =>{
+          if(err){
+              return res.send({
+                  success:false,
+                  message:'Error:Server error or Session not found'
+              })
+          }
+          if(sessions.length!=1){
+              return res.send({
+                  success:false,
+                  message:'Error:Invalid Session'
+              })
+          }else{
+                  //save to database
+                  const  newEvent = new Event();
+                   newEvent.datetime = datetime;
+                   newEvent.type = type;
+                   newEvent.drivingSide = drivingSide;
+                   newEvent.severity = severity;
+                   newEvent.kmPost = kmPost;
+                   newEvent.suburb = suburb;
+                   newEvent.sessionToken = sessionToken;
+                   newEvent.save()
+                  .then(() => 
+                      res.send({
+                      success:true,
+                      message:'Event submitted successfully.'
+                  })
+                  )
+                  .catch(err => res.send({
+                      success:false,
+                      message:'Error:Data Validation Error'
+                  })
+                  )
+  
+              }
+          }
+          )
+      });
 
 
 //List All Events
