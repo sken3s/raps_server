7f62233a30c5ccdf4857f64fc4d86c4d88250d88
Dulana Peiris <dulthar@gmail.com>
2021-03-25 23:19:41 +0530

    incident reporting changes

--- models/incidentReport.model.js
+++ models/incidentReport.model.js
@@ -5,11 +5,8 @@ const Schema = mongoose.Schema;
 const IncidentReportSchema = new Schema({
   datetime: { type: Date, default:Date.now},
   isAccident:{type: Boolean}, //True: Accident, False:Event
-  weather: { type: Boolean }, ////0:clear, 1:rain
-  vehicleType: { type: Number }, //0:car, 1:hv, 2:dualpurpose
   drivingSide: { type: Boolean }, //0:cmbtomatara, 1:mataratocmb
   kmPost: { type: Number, min:0, max:127},
-  suburb: { type: Number, min:0, max:10},
   status: { type: Number, min:0,max:2},  //status of accident: 1:reported,2:eTeam dispatched,3:handled },
   lat: { type: String },
   lng: { type: String },
